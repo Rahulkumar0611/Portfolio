7fb47c2218e6662696bec4d6685f9e4611db074c
Rahul <kumarrahul.sc@gmail.com>
2024-03-10 08:09:20 +0530

    code updated

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var typed = new Typed("#text", {
-    strings: ["Frontend Developer", "Photographer", "Gamer"],
+    strings: ["Frontend Developer", "Web designer", "java developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true,
 });
 var typed = new Typed("#text1", {
-    strings: ["Frontend Developer", "Photographer", "Gamer"],
+    strings: ["Frontend Developer", "Web designer", "java developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
